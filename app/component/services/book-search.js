1dd506ddf03e353e73bd7b1b12bc5ee34c5dc89c
yuvaraj.gunisetti <gyuvaraj10@gmail.com>
2019-03-15 18:21:20 +0000

    updated the code

--- app/component/services/book-search.js
+++ app/component/services/book-search.js
@@ -1,23 +1,15 @@
 var booksModule = angular.module('books');
 
-var bookSearch = function ($resource, $q) {
+var bookSearch = function ($http, $q) {
 
-    var Search = $resource('/books/search/:query', {query: '@query'});
-
-    this.getBooks = function (searchString) {
-        var promise = Search.query({query:searchString}).$promise;
-        return new Promise(function (resolve, reject) {
-            promise.then(function (response) {
-                var books = response.filter(function (item) {
-                    return item != {};
-                });
-                resolve(books);
-            }).catch(function (error) {
-                reject(error);
-            });
+    this.getBooks = function () {
+        return $http.get('http://localhost:8080/accounts/fixed-savers').then(function(response) {
+            return response.data;
+        }).catch(function(error) {
+            return error;
         });
     };
 
 };
 
-booksModule.service('bookSearch',['$resource', '$q', bookSearch]);
+booksModule.service('bookSearch',['$http', '$q', bookSearch]);
